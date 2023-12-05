c91dacd0d4bec58cfa594b5f7dfdc44b29ae5e77
Frustman <48879350+Frustman@users.noreply.github.com>
2023-12-05 17:34:49 +0900

    Feat : Lobby

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -1,18 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField nicknameField;
+    public TMP_InputField roomNameField;
+
+    public TextMeshProUGUI text;
+    public void Connect()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
+        PhotonNetwork.LocalPlayer.NickName = nicknameField.text;
+        Debug.Log("서버 접속 완료.");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("서버 연결 끊김.");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비 입장.");
+    }
+
+    public string GenerateRoomName()
+    {
+        string roomName = "";
+        string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         
+        for (int i = 0; i < 12; i++)
+        {
+            int randomIndex = Random.Range(0, characters.Length); // 문자열 내에서 무작위 인덱스 선택
+            roomName += characters[randomIndex]; // 선택된 문자를 방 이름에 추가
+        }
+
+        return roomName;
+    }
+    
+    public void CreateRoom()
+    {
+        string roomName = GenerateRoomName();
+        PhotonNetwork.CreateRoom(roomName, new RoomOptions());
+        Debug.Log(roomName);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(roomNameField.text);
+    }
+
+    private void Update()
+    {
+        text.text = PhotonNetwork.LocalPlayer.NickName;
     }
 }
